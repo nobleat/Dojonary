cddc5d085d184e387f376a7fa4739cf561cc98b7
nobleat <88677746+nobleat@users.noreply.github.com>
2021-10-01 20:47:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("JavaScript is connected");
+
+
+function log_switch(element){
+    element.innerText = "Logout";
+}
+
+function message() {
+    alert('Ninja was liked')
+}
+
+let accept = document.querySelector('#def_btn')
+function add_def() {
+    accept.remove()
+}
+
+
+
+
